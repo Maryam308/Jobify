132a32dd7c0643644425f1df97ec62cd48e31ac2
Zai-Al <zainabalawi08@gmail.com>
2024-11-28 18:01:51 +0300

    Editing Seeker, Job, and Employer classes, and adding methods in  Employer Class

--- Jobify/Models/Job.swift
+++ Jobify/Models/Job.swift
@@ -9,45 +9,78 @@ import Foundation
 
 
 struct Job {
+    // Auto-generated variables
     static var jobIdCounter = 0
     var jobId: Int
-    var company: String
-    var date: Date
+    var date: String
     var time: String
+    // Create a Calendar instance to format the date and time
+    var calendar = Calendar.current
+
+    // Passed variables
+    var title: String
+    var company: Employer
     var level: String
     var location: String
-    var description: String
+    var desc: String
     var requirement: String
-    //var applications: [Application] = []
+    var extraAttachments: Data? = nil
+    var employmentType: EmploymentType
     var deadline: Date
-    
-    
+
+    var applications: [JobApplication] = []
+
     // Custom initializer
-    init(company: String, date: Date, time: String, level: String, location: String, description: String, requirement: String, deadline: Date) {
+    init(
+        title: String,
+        company: inout Employer,
+        level: String,
+        location: String,
+        desc: String,
+        requirement: String,
+        extraAttachments: Data?,
+        employmentType: EmploymentType,
+        deadline: Date
+    ) {
+        // Auto-generated variables
         // Increment the counter and assign it as jobId
         Job.jobIdCounter += 1
         self.jobId = Job.jobIdCounter
-        
+
+        // Get the current date and time
+        let currentDate = Date()
+
+        // Extract the date components for the date
+        let dateComponents = self.calendar.dateComponents([.day, .month], from: currentDate)
+        let timeComponents = self.calendar.dateComponents([.hour, .minute], from: currentDate)
+
+        // Create date and time variables and convert date and time components to string
+        self.date = String(format: "%02d-%02d", dateComponents.day ?? 0, dateComponents.month ?? 0)
+        self.time = String(format: "%02d:%02d", timeComponents.hour ?? 0, timeComponents.minute ?? 0)
+
         // Assign other properties
+        self.title = title
         self.company = company
-        self.date = date
-        self.time = time
         self.level = level
         self.location = location
-        self.description = description
+        self.desc = desc
         self.requirement = requirement
+        self.employmentType = employmentType
         self.deadline = deadline
+
+        // Handle extraAttachments
+        if let attachment = extraAttachments {
+            self.extraAttachments = attachment
+        } else {
+            self.extraAttachments = nil
+        }
     }
-    
-}
-
-
-
-enum EmploymentType: String {
-    case fullTime = "Full-Time"
-    case partTime = "Part-Time"
-    case intern = "Intern"
-    case contract = "Contract"
+    enum EmploymentType: String {
+        case fullTime = "Full-Time"
+        case partTime = "Part-Time"
+        case intern = "Intern"
+        case contract = "Contract"
+    }
 }
 
 
