1b5af33e0a29fb834c7319bd29ef9e3827973f2f
Maryam308 <mariam601me@gmail.com>
2024-12-08 02:02:58 +0300

    started in cv builder views - need modifications ( scrolling and preview )

---
+++ Jobify/Controllers/CVBuilderEditorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CVBuilderEditorViewController.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 08/12/2024.
+//
+
+import UIKit
+
+class CVBuilderEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
