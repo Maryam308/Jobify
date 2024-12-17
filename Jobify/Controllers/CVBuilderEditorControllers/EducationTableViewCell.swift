178bf4b6611e1b9b0e610706de6d201d0dda53c6
Maryam308 <mariam601me@gmail.com>
2024-12-17 15:36:49 +0300

    cv validation almost done :)

---
+++ Jobify/Controllers/CVBuilderEditorControllers/EducationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EducationTableViewCell.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 16/12/2024.
+//
+
+import UIKit
+
+class EducationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
