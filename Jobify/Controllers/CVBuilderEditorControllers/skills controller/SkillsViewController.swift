3e4728e0cfd6f7753a0a91fb9f7229ef0c8c3575
Maryam308 <mariam601me@gmail.com>
2024-12-19 16:58:01 +0300

    cv builder

---
+++ Jobify/Controllers/CVBuilderEditorControllers/skills controller/SkillsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SkillsViewController.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 19/12/2024.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
