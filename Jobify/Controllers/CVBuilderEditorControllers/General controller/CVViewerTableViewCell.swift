64714366b6aa9f42ad623bb0c5d052ef12cd7c1c
Maryam308 <mariam601me@gmail.com>
2024-12-21 03:03:41 +0300

    working on CV builder - edit cv, cv view image, favorite cv should be fixed & validation for job title should be added

---
+++ Jobify/Controllers/CVBuilderEditorControllers/General controller/CVViewerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CVViewerTableViewCell.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 20/12/2024.
+//
+
+import UIKit
+
+class CVViewerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
