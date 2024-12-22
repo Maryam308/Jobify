652342bafa10e963f6271fdf8daa570aaf0d446d
Maryam308 <mariam601me@gmail.com>
2024-12-22 13:42:10 +0300

    collection view

---
+++ Jobify/Controllers/CareerResourcesControllers/CareerPathCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CareerPathCollectionViewCell.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 22/12/2024.
+//
+
+import UIKit
+
+class CareerPathCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
