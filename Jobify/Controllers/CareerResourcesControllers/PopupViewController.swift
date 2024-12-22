fa51bd5fa25f1d2beecfe0b43a912b77ed361502
Maryam308 <mariam601me@gmail.com>
2024-12-22 21:03:30 +0300

    popup

---
+++ Jobify/Controllers/CareerResourcesControllers/PopupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopupViewController.swift
+//  Jobify
+//
+//  Created by Maryam Yousif on 22/12/2024.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
