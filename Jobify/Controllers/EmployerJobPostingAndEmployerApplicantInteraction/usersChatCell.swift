7f013b37a5130db4f912a945b93af1b985159e0e
MariamAhmed004 <mariamisa5w@gmail.com>
2024-12-25 17:18:58 +0300

    user messages appear remains the single message screen

---
+++ Jobify/Controllers/EmployerJobPostingAndEmployerApplicantInteraction/usersChatCell.swift
@@ -0,0 +1,27 @@
+//
+//  AddNewJobPost.swift
+//  Jobify
+//
+//  Created by Maryam Ahmed on 21/12/2024.
+//
+
+import UIKit
+
+class usersChatCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var lablOtherUserName: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+    
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+}
+
+  
